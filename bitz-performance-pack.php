0e58f727909613973cea91d050f914701bd3abe0
Volnus <scotth0407@gmail.com>
2016-01-28 18:09:17 -0600

    Create bitz-performance-pack.php

---
+++ bitz-performance-pack.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Plugin Name:       Bitz Performance Pack
+ * Plugin URI:        http://thearcadecorner.com
+ * Description:       This plugin makes several modifications to the WordPress admin and the Frontend to improve the experience and performance of the website.
+ * Version:           1.0
+ * Author:            Scott Hartley
+ * Author URI:        http://thearcadecorner.com
+*/
+
+// Remove query string from static files
+function remove_cssjs_ver( $src ) {
+ if( strpos( $src, '?ver=' ) )
+ $src = remove_query_arg( 'ver', $src );
+ return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+// Turn Off Admin Bar
+add_filter('show_admin_bar', '__return_false');
+
+// Remove query string from static files
+function remove_cssjs_ver( $src ) {
+ if( strpos( $src, '?ver=' ) )
+ $src = remove_query_arg( 'ver', $src );
+ return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+// Turn Off Admin Bar
+add_filter('show_admin_bar', '__return_false');
+
+
+// Optimize WooCommerce
+add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
+ 
+function child_manage_woocommerce_styles() {
+ 
+	//first check that woo exists to prevent fatal errors
+	if ( function_exists( 'is_woocommerce' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+wp_dequeue_style( 'woocommerce-layout' );
+wp_dequeue_style( 'woocommerce-smallscreen' );
+wp_dequeue_style( 'woocommerce-general' );
+wp_dequeue_script( 'wc-add-to-cart' );
+wp_dequeue_script( 'wc-cart-fragments' );
+wp_dequeue_script( 'woocommerce' );
+wp_dequeue_script( 'jquery-blockui' );
+wp_dequeue_script( 'jquery-placeholder' );
+		}
+	}
+ 
+}
+
+// Deregister Contact Form 7 styles
+add_action( 'wp_print_styles', 'aa_deregister_styles', 100 );
+function aa_deregister_styles() {
+    if ( ! is_page( 'contact-us' ) ) {
+        wp_deregister_style( 'contact-form-7' );
+    }
+}
+
+// Deregister Contact Form 7 JavaScript files on all pages without a form
+add_action( 'wp_print_scripts', 'aa_deregister_javascript', 100 );
+function aa_deregister_javascript() {
+    if ( ! is_page( 'contact-us' ) ) {
+        wp_deregister_script( 'contact-form-7' );
+    }
+}
